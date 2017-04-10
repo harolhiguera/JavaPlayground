c827c4706b076792d7c48a675f51e93785280f0c
Harol Dev <haroldev@Harols-iMac.local>
2017-04-10 16:23:30 +0900

    Set pagination size for page requests

--- src/main/java/com/beep/course/CourseRepository.java
+++ src/main/java/com/beep/course/CourseRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long>{
 	
-	// By adding this line 
+	@RestResource(rel = "title-contains", path = "containsTitle")
 	Page<Course> findByTitleContaining(@Param("title") String title, Pageable page);
-  
+	
 }
  
