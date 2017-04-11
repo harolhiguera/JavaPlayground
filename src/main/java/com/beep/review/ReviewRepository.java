b88a412b5d6f5ba87bc383750d213ba9081fbaeb
Harol Dev <haroldev@Harols-iMac.local>
2017-04-11 12:43:15 +0900

    Authorize deletion of review of the same user that wrote it and also for User with Admin proviledges

--- src/main/java/com/beep/review/ReviewRepository.java
+++ src/main/java/com/beep/review/ReviewRepository.java
@@ -1,8 +1,18 @@
 package com.beep.review;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long>{
 
-	
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.userName == authentication.name")
+	void delete(@Param("id") Long id);
+
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.userName == authentication.name")
+	void delete(@Param("review") Review entity);
+
+	//TODO: Check out the documentation for authentication security expressions !!!
 }
